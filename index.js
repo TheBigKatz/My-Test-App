00c3eb953635290ba57924d285ef712e131c0504
Andrew Katz <andrewkz123@yahoo.com>
2016-06-24 12:04:20 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.get('/', function(request, response) {
+  res.render('hello', {
+    title: 'Hello',
+    content: 'Hello, world!' 
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
